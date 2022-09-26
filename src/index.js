ef5efd0f4dec117a69c8cdac86b344cb0b4e80c2
virgiliusnanamanek <vickynana034@gmail.com>
2022-09-26 20:03:13 +0700

    add button cancel to the form

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const formBook = document.querySelector('#form-book');
 const judul = document.querySelector('#judul');
 const penulis = document.querySelector('#penulis');
 const tahun = document.querySelector('#tahun');
+const btnCancel = document.querySelector('#btn-cancel');
 
 function addBooks(){
    formBook.classList.remove('hidden');
 }
 
+function cancelBooks(){
+   formBook.classList.add('hidden');
+}
+
 addBook.addEventListener('click', addBooks);
+btnCancel.addEventListener('click', cancelBooks);
